eb3b8bcb55f4bf15fb7ef730cb045e4f644e3f06
Lorenc Pekaj <lorenc@kobecreations.com>
2019-04-10 15:21:01 +1000

    noob

--- src/modules/crypto.js
+++ src/modules/crypto.js
@@ -10,41 +10,31 @@ bot.cmd('crypto', ([primary = 'BTC', secondary = 'USD'], _message, { channel }) 
     secondary = secondary.toUpperCase()
 
     request({
-        url: `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${primary}&tsyms=${secondary}&api_key=d0bff0f914f0b86c544f6ee1f959ef5e18cf978135988d24cb493442551777fd`,
+        url: `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${primary}&tsyms=${secondary}&api_key=`,
         json: true
     }, (err, _res, body) => {
         try {
             const content = body.RAW[primary][secondary]
-            // console.log(JSON.stringify(body))
-            bot.msgAttachment('bot-test', '', {
-                "attachments": [
+            bot.msgAttachment(meta., '', {
+                'attachments': [
                     {
-                        "fallback": "Required plain-text summary of the attachment.",
-                        "color": "#" + make_colour(6),
-                        // "pretext": "Optional text that appears above the attachment block",
-                        // "author_name": "Bobby Tables",
-                        // "author_link": "http://flickr.com/bobby/",
-                        // "author_icon": "http://flickr.com/icons/bobby.jpg",
-                        // "title": "Slack API Documentation",
-                        // "title_link": "https://api.slack.com/",
-                        "title": `${primary}/${secondary} Price Information`,
-                        "fields": [
+                        'fallback': 'Required plain-text summary of the attachment.',
+                        'color': '#' + make_colour(6),
+                        'title': `${primary}/${secondary} Price Information`,
+                        'fields': [
                             {
-                                "title": `Price`,
-                                "value": content.PRICE.toLocaleString() + " " + content.TOSYMBOL,
-                                "short": true
+                                'title': `Price`,
+                                'value': content.PRICE.toLocaleString() + ' ' + content.TOSYMBOL,
+                                'short': true
                             },
                             {
-                                "title": `Market Cap`,
-                                "value": content.MKTCAP.toLocaleString() + " " + content.TOSYMBOL,
-                                "short": true
+                                'title': `Market Cap`,
+                                'value': content.MKTCAP.toLocaleString() + ' ' + content.TOSYMBOL,
+                                'short': true
                             }
                         ],
-                        // "image_url": "https://www.cryptocompare.com/media/19633/btc.png",
-                        "thumb_url": `https://www.cryptocompare.com/${content.IMAGEURL}`,
-                        // "footer": "Slack API",
-                        // "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                        "ts": +Date.now()/1000
+                        'thumb_url': `https://www.cryptocompare.com/${content.IMAGEURL}`,
+                        'ts': +Date.now()/1000
                     }
                 ]
             });
