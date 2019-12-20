c887297021b3974e637b7cc30a3be3588279ead0
Toofifty <alex@matho.me>
2019-12-20 16:21:50 +1100

    trivia cats

--- src/modules/trivia.ts
+++ src/modules/trivia.ts
@@ -22,6 +22,31 @@ interface ApiQuestion {
     incorrect_answers: string[];
 }
 
+const categories = {
+    'General Knowledge': 9,
+    'Entertainment: Books': 10,
+    'Entertainment: Film': 11,
+    'Entertainment: Music': 12,
+    'Entertainment: Musicals & Theatre': 13,
+    'Entertainment: Television': 14,
+    'Entertainment: Video Games': 15,
+    'Entertainment: Board Games': 16,
+    'Science & Nature': 17,
+    'Science: Computers': 18,
+    'Science: Mathematics': 19,
+    Mythology: 20,
+    Sports: 21,
+    Geography: 22,
+    History: 23,
+    Politics: 24,
+    Art: 25,
+    Celebrities: 26,
+    Animals: 27,
+    Vehicles: 28,
+    'Entertainment: Comics': 29,
+    'Science: Gadgets': 30,
+};
+
 const incorrect = ({ send, user }: CommandContext) => {
     const badGuesses = userdata
         .update(user, 'trivia_bad_guesses', g => g + 1, 0)
